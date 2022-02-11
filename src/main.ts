fc04ac90abe299fc1f85161076ebe0ca7a0f1439
tanyukang <tanyukang888@163.com>
2022-02-11 17:03:36 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 console.log("ok");
 
-console.log("start");
 
 const qwer = 23;
-createApp(App).mount("#app");
+  createApp(App).mount("#app")
+
 console.log(33);
