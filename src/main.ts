7379fa19b3adaf3163f697a6ebd203b5cd9cf1f3
tangjian1891 <tangjian1891@163.com>
2022-02-11 17:09:39 +0800

    提交lint操作

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 console.log("ok");
-
-
+console.log("3131");
+console.log("3131");
 const qwer = 23;
-  createApp(App).mount("#app")
+createApp(App).mount("#app");
 
 console.log(33);
