b01522e176a465ca5fd3f957d5c1efcb9dbb3eb3
tangjian1891 <tangjian1891@163.com>
2022-02-11 16:54:35 +0800

    lint-staged

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 console.log("ok");
-console.log("start")
-let qwer=23
-  createApp(App).mount("#app");
+
+console.log("start");
+
+const qwer = 23;
+createApp(App).mount("#app");
+console.log(33);
